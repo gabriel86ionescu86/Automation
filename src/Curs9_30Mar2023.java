9909757b0d69c232fb785532339ce8d3ae86cc7c
Domino <gabriel86ionescu86@gmail.com>
2023-03-30 21:01:20 +0300

    update cu exercitii curs 9

--- src/Curs9_30Mar2023.java
+++ src/Curs9_30Mar2023.java
@@ -2,11 +2,61 @@
 import java.util.Scanner;
 
 public class Curs9_30Mar2023 {
-    public static void main(String[] args) {
-        // Pachete incorporate(predefinite) si pachete custom(definite de noi)
-        Scanner  obj = new Scanner(System.in);
+   /* protected String str;
 
+    Curs9_30Mar2023(){
+        str = "This";
+    }}
 
-
+    class Copil1 extends Curs9_30Mar2023{
+        Copil1(){
+            str = str.concat(" is");
+        }
     }
+    class Copil2 extends Copil1{
+        Copil2(){
+            str = str.concat(" Curs9_30Mar2023 Inheritance");
+        }
+    }
+    class Copil3 extends Copil2{
+        Copil3(){
+            str = str.concat(" Example.");
+        }
+        void display(){
+            System.out.println(str);
+    }
+
+    public static void main(String[] args) {
+        Copil3 cop = new Copil3();
+        cop.display();
+
+        }*/
 }
+
+   class BaseClass{
+    void displayA(){
+        System.out.println("Continut clasa de baza");
+    }
+
+    static class A extends BaseClass{
+        void displayB(){
+            System.out.println("Continut clasa A");
+        }
+    }
+   static class B extends BaseClass{
+       void displayC(){
+           System.out.println("Continut clasa B");
+       }
+   }
+
+       public static void main(String[] args) {
+        B b = new B();
+        b.displayA();
+        b.displayC();
+
+       A a = new A();
+       a.displayA();
+       a.displayB();
+       }
+    }
+
--- src/pachet1/Animal.java
+++ src/pachet1/Animal.java
@@ -16,8 +16,16 @@ class Caine extends Animal{
     public void scoateSunet(){
         System.out.println("Ham-Ham!");
     }
+}
+
+class cainePrietenos extends Caine{
     public void estePrietenos(){
         System.out.println("Este foarte prietenos si nu musca.");
+    }
+}
+class caineNeprietenos extends Caine{
+    public void nuEstePrietenos(){
+        System.out.println("Atentie, acest caine musca!");
     }
 }
 
@@ -32,20 +40,32 @@ class Pisica extends Animal {
     }
 
     public static void main(String[] args) {
-        Caine catel1 = new Caine();
+        /*Caine catel1 = new Caine();
         catel1.nume = "Scooby";
         catel1.sex = 'M';
         catel1.greutate = 10.2;
         System.out.println(catel1.tipAnimal + " " + catel1.nume + " are " + catel1.age +"ani, " + catel1.greutate + "kg si este de sex " + catel1.sex);
         catel1.scoateSunet();
-        catel1.estePrietenos();
-        System.out.println("---------------------------");
+        catel1.estePrietenos();//*/
+
+        cainePrietenos catel2 = new cainePrietenos();
+        catel2.nume = "Azorel";
+        System.out.println("Cum este " + catel2.nume);
+        catel2.estePrietenos();
+
+
+        caineNeprietenos catel3 = new caineNeprietenos();
+        catel3.nume = "Azorel";
+        System.out.println("Cum este " + catel3.nume);
+        catel3.nuEstePrietenos();
+
+       /* System.out.println("---------------------------");
         Pisica pisica1 = new Pisica();
         pisica1.nume = "Luna";
         pisica1.sex = 'F';
         pisica1.greutate = 3.2;
         System.out.println(pisica1.tipAnimal+ " " + pisica1.nume + " are " +pisica1.age + "ani,  " + pisica1.greutate + "kg si este de sex " + pisica1.sex);
         pisica1.scoateSunet();
-        pisica1.estePrietenos();
+        pisica1.estePrietenos();*/
     }
 }
