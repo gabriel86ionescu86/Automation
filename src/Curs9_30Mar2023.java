4163236cefda0dbb89eec0c7a2879ebf6ec47c8c
Domino <gabriel86ionescu86@gmail.com>
2023-03-30 20:18:14 +0300

    update cu exercitii sambata Clase

---
+++ src/Curs9_30Mar2023.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class Curs9_30Mar2023 {
+    public static void main(String[] args) {
+        // Pachete incorporate(predefinite) si pachete custom(definite de noi)
+        Scanner  obj = new Scanner(System.in);
+
+
+
+    }
+}
---
+++ src/pachet1/Animal.java
@@ -0,0 +1,51 @@
+package pachet1;
+
+class Animal {
+    public String nume;
+    public char sex;
+    public double greutate;
+    protected String tipAnimal;
+    protected int age;
+    protected void scoateSunet(){}
+    protected void estePrietenos(){}
+}
+
+class Caine extends Animal{
+    public String tipAnimal = "Catel";
+    public int age = 4;
+    public void scoateSunet(){
+        System.out.println("Ham-Ham!");
+    }
+    public void estePrietenos(){
+        System.out.println("Este foarte prietenos si nu musca.");
+    }
+}
+
+class Pisica extends Animal {
+    public String tipAnimal = "Pisica";
+    int age = 4;
+    public void scoateSunet() {
+        System.out.println("Miau!");
+    }
+    public void estePrietenos(){
+        System.out.println("Atentie zgarie! Nu insistati cu mangaierile");
+    }
+
+    public static void main(String[] args) {
+        Caine catel1 = new Caine();
+        catel1.nume = "Scooby";
+        catel1.sex = 'M';
+        catel1.greutate = 10.2;
+        System.out.println(catel1.tipAnimal + " " + catel1.nume + " are " + catel1.age +"ani, " + catel1.greutate + "kg si este de sex " + catel1.sex);
+        catel1.scoateSunet();
+        catel1.estePrietenos();
+        System.out.println("---------------------------");
+        Pisica pisica1 = new Pisica();
+        pisica1.nume = "Luna";
+        pisica1.sex = 'F';
+        pisica1.greutate = 3.2;
+        System.out.println(pisica1.tipAnimal+ " " + pisica1.nume + " are " +pisica1.age + "ani,  " + pisica1.greutate + "kg si este de sex " + pisica1.sex);
+        pisica1.scoateSunet();
+        pisica1.estePrietenos();
+    }
+}
